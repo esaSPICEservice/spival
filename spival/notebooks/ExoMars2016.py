63a7d72816fa0c59d66a6c1c8e9c3b603b25c2bb
Marc Costa <marc.costa@sciops.esa.int>
2019-02-20 20:34:17 +0100

    Files for beta version of spival.

--- spival/notebooks/ExoMars2016.py
+++ spival/notebooks/ExoMars2016.py
@@ -21,14 +21,84 @@
    "source": [
     "import spiops\n",
     "\n",
-    "spiops.load('{metakernel}')                  # The meta-kernel is loaded \n",
+    "spiops.load('{metakernel}')                    # The meta-kernel is loaded \n",
     "\n",
-    "start_time = '{start_time}'                  # Start time\n",
-    "finish_time ='{finish_time}'                 # Finish time\n",
+    "start_time = '{start_time}'                                # Start time\n",
+    "finish_time ='{finish_time}'                                # Finish time\n",
     "\n",
     "interval = spiops.TimeWindow(start_time, finish_time,resolution=60) # spiops object TimeWindow generated\n",
     "mars = spiops.Target('MARS', time=interval, frame='IAU_MARS')       # spiops object Target Mars generated\n",
-    "tgo = spiops.Observer('TGO', time=interval, target=mars)            # spiops object Observer TGO generated"
+    "tgo = spiops.Observer('TGO', time=interval, target=mars)            # spiops object Observer TGO generated\n\n",
+    "spiops.ck_coverage_timeline('{metakernel}')\n",
+    "spiops.spk_coverage_timeline('{metakernel}')"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "## Attitude Error\n",
+    "Comparison of -Y axis orientation between predicted and measured attitude in milidegrees "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "resolution = 16\n",
+    "\n",
+    "measured_ck = '{skd_path}/ck/{measured_ck}'\n",
+    "predicted_ck = '{skd_path}/ck/{predicted_ck}'\n",
+    "\n",
+    "spiops.ckdiff_error(measured_ck, predicted_ck, 'TGO_SPACECRAFT', 'J2000', resolution, 0.0001, \n",
+    "                    notebook=True, plot_style='circle', boresight=[0,-1,0])"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "## S/C Structures Orientation\n",
+    "Solar Array Orientation, High Gain Antenna Orientation\n",
+    "\n"
+   ]
+  },
+{
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "tgo.Plot('sa_ang', notebook=True)              # Solar Array (SA) Angles"
+   ]
+  },
+{
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "tgo.Plot('saa_sa', notebook=True)              # SA Solar Aspect Angle"
+   ]
+  },
+{
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "tgo.Plot('hga_earth', notebook=True)           # High Gain Antenna - Earth Angle"
+   ]
+  },
+{
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "tgo.Plot('hga_angles', notebook=True)          # High Gain Antenna Angles"
    ]
   },
   {
@@ -85,74 +155,12 @@
    ]
   },
   {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "## Attitude Error\n",
-    "Comparison of -Y axis orientation between predicted and measured attitude in milidegrees "
-   ]
-  },
-  {
-   "cell_type": "code",
+   "cell_type":       "code",
    "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "resolution = 16\n",
-    "tolerance =  0.0001  #[DEG]\n",
-    "spacecraft_frame = 'TGO_SPACECRAFT'\n",
-    "target_frame =     'J2000'\n",
-    "\n",
-    "measured_ck = '{skd_path}/ck/{measured_ck}'\n",
-    "predicted_ck = '{skd_path}/ck/{predicted_ck}'\n",
-    "\n",
-    "spiops.ckdiff_error(measured_ck, predicted_ck, spacecraft_frame, target_frame, resolution, tolerance, \n",
-    "                    notebook=True, plot_style='circle', boresight=[0,-1,0])"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "## S/C Structures Orientation\n",
-    "Solar Array Orientation, High Gain Antenna Orientation\n",
-    "\n"
-   ]
-  },
-{
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "tgo.Plot('sa_ang', notebook=True)              # Solar Array (SA) Angles"
-   ]
-  },
-{
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "tgo.Plot('saa_sa', notebook=True)              # SA Solar Aspect Angle"
-   ]
-  },
-{
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "tgo.Plot('hga_earth', notebook=True)           # High Gain Antenna - Earth Angle"
-   ]
-  },
-{
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "tgo.Plot('hga_angles', notebook=True)          # High Gain Antenna Angles"
+   "metadata":        {},
+   "outputs":         [],
+   "source":          [
+    "tgo.Plot('beta_angle', notebook=True)         # Beta Angle"
    ]
   }
  ],
