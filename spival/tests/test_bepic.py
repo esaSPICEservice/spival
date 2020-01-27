4a861299a113a22687f1681f7842daae01b3f82b
Marc Costa <marc.costa@esa.int>
2020-01-27 15:49:16 +0100

    Improvements to usage and for the pipeline.

--- spival/tests/test_bepic.py
+++
@@ -1,7 +0,0 @@
-from spival import write_BepiColombo
-import json
-
-with open('test_bepic.json') as f:
-    config = json.load(f)
-
-write_BepiColombo(config)
--- spival/tests/test_mex.py
+++
@@ -1,7 +0,0 @@
-from spival import write_MarsExpress
-import json
-
-with open('test_mex.json') as f:
-    config = json.load(f)
-
-write_MarsExpress(config)
