10ab3685927a116008cbf6b3e536e25ddcf8c73d
Ricardo Valles <rvalles@sciops.esa.int>
2021-04-28 18:03:12 +0200

    SPICEPCR-627: Improved header information in validation notes.

--- spival/utils/utils.py
+++ spival/utils/utils.py
@@ -1,6 +1,7 @@
 from os import remove, path
 import bs4
 
+
 def kernel_extension2type(kernel_extension):
     kernel_type_map = {
         "TI": "IK",
@@ -40,57 +41,54 @@ def fill_template(template,
                   file,
                   replacements,
                   cleanup=False):
+    #
+    # If the temp   late file is equal to the output file then we need to create a temporary template - which will be
+    # a duplicate - in order to write in the file. A situation where we would like to have them be the same is
+    # for example if we call this function several times in a row, replacing keywords in the template in steps
+    #
+    if template == file:
+        with open(file, 'r') as f:
+            with open('fill_template.temp', "w+") as t:
+                for line in f:
+                    t.write(line)
 
+        template = 'fill_template.temp'
 
-   #
-   # If the temp   late file is equal to the output file then we need to create a temporary template - which will be
-   # a duplicate - in order to write in the file. A situation where we would like to have them be the same is
-   # for example if we call this function several times in a row, replacing keywords in the template in steps
-   #
-   if template == file:
-       with open(file, 'r') as f:
-           with open('fill_template.temp', "w+") as t:
-               for line in f:
-                   t.write(line)
+    with open(file, "w+") as f:
+        #
+        # Items are replaced as per correspondence in between the replacements dictionary
+        #
+        with open(template, 'r') as t:
+            for line in t:
+                if '{' in line:
+                    for k, v in replacements.items():
+                        if '{' + k + '}' in line: line = line.replace('{' + k + '}', v)
+                f.write(line)
 
-       template = 'fill_template.temp'
+    #
+    # If the option cleanup is set as true, we remove the keyword assignments in the filled templated which are
+    # unfilled (they should be optional)
+    #
+    if cleanup:
 
+        with open(file, 'r') as f:
+            with open('fill_template.temp', 'w+') as t:
+                for line in f:
+                    t.write(line)
 
-   with open(file, "w+") as f:
-       #
-       # Items are replaced as per correspondance in between the replacements dictionary
-       #
-       with open(template, 'r') as t:
-           for line in t:
-               if '{' in line:
-                   for k, v in replacements.items():
-                       if '{' + k + '}' in line: line = line.replace('{' + k + '}', v)
-               f.write(line)
+        template = 'fill_template.temp'
 
-               #
-               # If the option cleanup is set as true, we remove the keyword assignments in the filled templated which are
-               # unfilled (they should be optional)
-               #
-   if cleanup:
+        with open(file, 'w+') as f:
+            with open('fill_template.temp', 'r') as t:
+                for line in t:
+                    if '{' not in line:
+                        f.write(line)
 
-       with open(file, 'r') as f:
-           with open('fill_template.temp', 'w+') as t:
-               for line in f:
-                   t.write(line)
-
-       template = 'fill_template.temp'
-
-       with open(file, 'w+') as f:
-           with open('fill_template.temp', 'r') as t:
-               for line in t:
-                   if '{' not in line:
-                       f.write(line)
-
-                       #
-                       # The temporary files are removed
-                       #
-   if path.isfile('fill_template.temp'):
-       remove('fill_template.temp')
+    #
+    # The temporary files are removed
+    #
+    if path.isfile('fill_template.temp'):
+        remove('fill_template.temp')
 
 
 def post_process_html(html_file):
@@ -125,7 +123,9 @@ def post_process_html(html_file):
 
     # Generate a new html file with only the validation results
     soup2 = bs4.BeautifulSoup("", "html.parser")
-    soup2.append(val_res_div)
+    soup2.append(soup.find("h1"))
+    soup2.append(soup.find("p"))
+    soup2.find("a").clear()
     with open(html_file.replace(".html", "_results.html"), "w") as outf:
         outf.write(str(soup2))
 
