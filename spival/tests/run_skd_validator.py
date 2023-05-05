9e87d5b307a198dfe45f9b8bf31fc7e21af208d2
Ricardo Valles <Ricardo.Valles@ext.esa.int>
2023-05-05 18:11:12 +0200

    Revert "SPICEPCR-1117 and SPICEPCR-1121: Add Time correlation deviation check in SPIVAL, Update SPIVAL to reflect OPS meta-kernel data"
    This reverts commit 520041b1844d7d677cdd4e7c8e83d9a2fcb04e1d.

--- spival/tests/run_skd_validator.py
+++ spival/tests/run_skd_validator.py
@@ -2,4 +2,4 @@ from spival.core.skd import validate
 
 # TEST:
 if __name__ == '__main__':
-    validate(["/home/rvalles/spice/kernels/juice/kernels/dsk/*"])
+    validate(["/home/rvalles/spice/kernels/integral/kernels/*/*"])
